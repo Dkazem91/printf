cde381f8b31ef716bfab64e4b6cbd0ffb5955b50
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-02-24 07:57:54 -0800

    Add set flags for width and precision

--- holberton.h
+++ holberton.h
@@ -8,6 +8,8 @@
  * @width: width of field to print
  * @precision: precision of field to print
  * @length: modifies type length. h/l flags. 0 default, +1 per l, -1 per h
+ * @widthflag: tells whether width is set
+ * @precisionflag: tells whether precision is set
  * @left: left justify, '-'. 0 for right, 1 for left
  * @sign: force sign, '+'. 0 for no, 1 for yes
  * @space: space pad left side of positive numbers if leading 0s. 0 no 1 yes
@@ -20,6 +22,8 @@ typedef struct specifier
 	unsigned int width;
 	unsigned int precision;
 	int length;
+	char widthflag;
+	char precisionflag;
 	char left;
 	char sign;
 	char space;
