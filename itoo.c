ff40712ac065a19c7dc2cb077fd6cdcf5ec99a76
Dkazem91 <Dkazemian@gmail.com>
2018-02-26 20:37:06 +0000

    fixed itoo to work for long, long long

--- itoo.c
+++ itoo.c
@@ -7,30 +7,23 @@ char *itoo(va_list list)
 {
 	unsigned int digits, digitTest;
 	int count, i;
-	unsigned long mult, result;
 	char* output;
 
 	digits = va_arg(list, unsigned int);
 	count = 0;
-	mult = 1;
-	result = 0;
 	digitTest = digits;
 	while (digitTest > 0)
 	{
-		result += (digitTest % 8) * mult;
 		digitTest /= 8;
-		mult *=10;
 	        count++;
 	}
+	digitTest = digits;
 	output = malloc(sizeof(char) * count + 1);
-	mult /= 10;
-        for (i = 0; i < count; i++)
+        for (i = count - 1; i >= 0; i--)
         {
-                output[i] = ((result / mult) + '0');
-                result %= mult;
-		mult /= 10;
+                output[i] = ((digitTest % 8) + '0');
+                digitTest /= 8;
         }
         output[i] = '\0';
 	return(output);
-
 }
